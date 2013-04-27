c14938947334b561c70bb6648f05a41524b14df5
abrahamwl@gmail.com <abrahamwl@gmail.com@0c5cd833-a6f2-db63-4c2d-ceeb27597673>
2013-04-27 03:36:02 +0000

    Added short Actor description.

--- Killer Pigeons Dare/src/game/Actor.java
+++ Killer Pigeons Dare/src/game/Actor.java
@@ -2,10 +2,12 @@ package game;
 
 import org.newdawn.slick.Image;
 
+// The Actor class is the class inherited by all "characters" in the game. That is, the player character, all monsters, 
+// and other NPCs.
 public class Actor {
-	int x, y;
-	
+	int x, y; //Location in room coordinates.
+
 	String name;
-	
+
 	Image image = null;
 }
