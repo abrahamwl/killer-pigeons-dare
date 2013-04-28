7f5797f209c522425ce275db7cd19e9057b0c029
eric.holloway@gmail.com <eric.holloway@gmail.com@0c5cd833-a6f2-db63-4c2d-ceeb27597673>
2013-04-28 07:52:34 +0000

    more work on flameo

--- Killer Pigeons Dare/src/game/controller/FlameoController.java
+++ Killer Pigeons Dare/src/game/controller/FlameoController.java
@@ -15,16 +15,44 @@ public class FlameoController extends BasicController {
 		a = monster;
 	}
 	
+	int flameSpawnTicks = 4; // Number of ticks before creating new flame
+	int flameTime = 10; // How long a flame lives
+	
 	@Override
 	public Action chooseNextAction(Room room, GameContainer gc) {	
 		Actor t = room.game.hero;
+
+		flameTime--;
+		if(flameTime == 0) die();
 		
-		if (a.x > h.x) {
-			if (a.y > h.y) {
+		int distToHero = Math.abs(a.x - t.x) + Math.abs(a.y - t.y);
+		
+		if(distToHero > 10) return chooseMovement(room, t);
+		
+		if(distToHero > 5) return spawnFlame(room);
+		
+		return new ActionWait();
+	}
+
+	private void die() {
+		// TODO
+	}
+	
+	private Action spawnFlame(Room room) {
+		// TODO
+		
+		return null;
+	}
+	
+	// TODO remove moveAttack
+	
+	private Action chooseMovement(Room room, Actor t) {
+		if (a.x > t.x) {
+			if (a.y > t.y) {
 				if (!room.checkForTypeAt(a.x - 1, a.y - 1, Wall.class)) {
 					return moveAttack(room, Dir.NORTH_WEST);
 				} else {
-					if (a.x - h.x > a.y - h.y) {
+					if (a.x - t.x > a.y - t.y) {
 						if (!room.checkForTypeAt(a.x - 1, a.y, Wall.class)) {
 							return moveAttack(room, Dir.WEST);
 						} else {
@@ -38,11 +66,11 @@ public class FlameoController extends BasicController {
 						}
 					}
 				}
-			} else if (a.y < h.y) {
+			} else if (a.y < t.y) {
 				if (!room.checkForTypeAt(a.x - 1, a.y + 1, Wall.class)) {
 					return moveAttack(room, Dir.SOUTH_WEST);
 				} else {
-					if (a.x - h.x > h.y - a.y) {
+					if (a.x - t.x > t.y - a.y) {
 						if (!room.checkForTypeAt(a.x - 1, a.y, Wall.class)) {
 							return moveAttack(room, Dir.WEST);
 						} else {
@@ -59,12 +87,12 @@ public class FlameoController extends BasicController {
 			} else {
 				return moveAttack(room, Dir.WEST);
 			}
-		} else if (a.x < h.x) {
-			if (a.y > h.y) {
+		} else if (a.x < t.x) {
+			if (a.y > t.y) {
 				if (!room.checkForTypeAt(a.x + 1, a.y - 1, Wall.class)) {
 					return moveAttack(room, Dir.NORTH_EAST);
 				} else {
-					if (h.x - a.x > a.y - h.y) {
+					if (t.x - a.x > a.y - t.y) {
 						if (!room.checkForTypeAt(a.x + 1, a.y, Wall.class)) {
 							return moveAttack(room, Dir.EAST);
 						} else {
@@ -78,11 +106,11 @@ public class FlameoController extends BasicController {
 						}
 					}
 				}
-			} else if (a.y < h.y) {
+			} else if (a.y < t.y) {
 				if (!room.checkForTypeAt(a.x + 1, a.y + 1, Wall.class)) {
 					return moveAttack(room, Dir.SOUTH_EAST);
 				} else {
-					if (h.x - a.x > h.y - a.y) {
+					if (t.x - a.x > t.y - a.y) {
 						if (!room.checkForTypeAt(a.x+ 1, a.y, Wall.class)) {
 							return moveAttack(room, Dir.EAST);
 						} else {
@@ -100,9 +128,9 @@ public class FlameoController extends BasicController {
 				return moveAttack(room, Dir.EAST);
 			}
 		} else {
-			if (a.y > h.y) {
+			if (a.y > t.y) {
 				return moveAttack(room, Dir.NORTH);
-			} else if (a.y < h.y) {
+			} else if (a.y < t.y) {
 				return moveAttack(room, Dir.SOUTH);
 			} else {
 				return new ActionWait();
