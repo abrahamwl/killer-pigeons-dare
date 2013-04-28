ccfeece043e5f70bd8ad2a4526cd51bea1de3a8a
abrahamwl@gmail.com <abrahamwl@gmail.com@0c5cd833-a6f2-db63-4c2d-ceeb27597673>
2013-04-28 04:52:25 +0000

    Numpad support.

--- Killer Pigeons Dare/src/game/UserController.java
+++ Killer Pigeons Dare/src/game/UserController.java
@@ -16,17 +16,29 @@ public class UserController extends BasicController {
 	public Action chooseNextAction(Room room, GameContainer gc) {
 		Input input = gc.getInput();
 		
-		if (input.isKeyPressed(Input.KEY_RIGHT)) {
+		if (input.isKeyPressed(Input.KEY_RIGHT) || input.isKeyPressed(Input.KEY_NUMPAD6)) {
 			return moveAttack(room, Dir.EAST);
 		}
-		if (input.isKeyPressed(Input.KEY_DOWN)) {
+		if (input.isKeyPressed(Input.KEY_NUMPAD3)) {
+			return moveAttack(room, Dir.SOUTH_EAST);
+		}
+		if (input.isKeyPressed(Input.KEY_DOWN) || input.isKeyPressed(Input.KEY_NUMPAD2)) {
 			return moveAttack(room, Dir.SOUTH);
 		}
-		if (input.isKeyPressed(Input.KEY_LEFT)) {
+		if (input.isKeyPressed(Input.KEY_NUMPAD1)) {
+			return moveAttack(room, Dir.SOUTH_WEST);
+		}
+		if (input.isKeyPressed(Input.KEY_LEFT) || input.isKeyPressed(Input.KEY_NUMPAD4)) {
 			return moveAttack(room, Dir.WEST);
 		}
-		if (input.isKeyPressed(Input.KEY_UP)) {
+		if (input.isKeyPressed(Input.KEY_NUMPAD7)) {
+			return moveAttack(room, Dir.NORTH_WEST);
+		}
+		if (input.isKeyPressed(Input.KEY_UP) || input.isKeyPressed(Input.KEY_NUMPAD8)) {
 			return moveAttack(room, Dir.NORTH);
+		}
+		if (input.isKeyPressed(Input.KEY_NUMPAD9)) {
+			return moveAttack(room, Dir.NORTH_EAST);
 		}
 		
 		return ActionNoneYet.INSTANCE;
