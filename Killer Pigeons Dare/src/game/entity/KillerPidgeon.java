73c6e56db015a36fc979cdb7db3ebf57ca18a467
abrahamwl@gmail.com <abrahamwl@gmail.com@0c5cd833-a6f2-db63-4c2d-ceeb27597673>
2013-04-28 08:20:17 +0000

    Added KillerPidgeon.

---
+++ Killer Pigeons Dare/src/game/entity/KillerPidgeon.java
@@ -0,0 +1,24 @@
+package game.entity;
+
+import game.Ability;
+import game.Actor;
+import game.controller.AttackController;
+
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
+
+public class KillerPidgeon extends Actor {
+	public KillerPidgeon () {
+		super("Killer Pidgeon", 1);
+		controller = new AttackController(this);
+		abilities.add(new Ability(Ability.Type.FLYING));
+		SpriteSheet sheet;
+		try {
+			sheet = new SpriteSheet("res/game.png", 63, 63, 1);
+			image = sheet.getSubImage(4, 0);
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
+	}
+}
