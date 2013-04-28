49a26397084da9e7f00a626cbfc63da1222b44f7
eric.holloway@gmail.com <eric.holloway@gmail.com@0c5cd833-a6f2-db63-4c2d-ceeb27597673>
2013-04-28 02:17:54 +0000

    Added level editor, updated TODOs

--- Killer Pigeons Dare/src/game/Game.java
+++ Killer Pigeons Dare/src/game/Game.java
@@ -16,7 +16,7 @@ public class Game extends BasicGame {
 
 	public static void main(String[] args) throws SlickException {
 		AppGameContainer app = new AppGameContainer(new Game("Killer Pigeons RPG"));
-		app.setDisplayMode(800, 600, false);
+		app.setDisplayMode(512, 512, false);
 		app.start();
 	}
 
--- Killer Pigeons Dare/src/game/Room.java
+++ Killer Pigeons Dare/src/game/Room.java
@@ -13,43 +13,61 @@ public class Room {
 	ArrayList<Entity> ent;
 	ArrayList<Entity> waiting = new ArrayList<Entity>();
 	GameContainer gc;
-	
+
+	// This function is passed a series of class names representing entities.  Columns are broken up by commas, rows broken up by semicolons.
+	public Room (String roomString) {
+		try {
+			String[][] roomGrid = null;
+			String[] roomRow = null;
+			ent = new ArrayList<Entity>();
+			roomString = roomString.replaceAll("\\s", ""); // Remove all whitespace
+
+			roomRow = roomString.split(";");
+			roomGrid = new String[roomRow.length][];
+			for(int r = 0; r < roomRow.length; r++) roomGrid[r] = roomRow[r].split(",");
+
+			for(int r = 0; r < roomGrid.length; r++) for(int c = 0; c < roomGrid[r].length; c++) ent.add((Entity) Class.forName(roomGrid[r][c]).newInstance());
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+	}
+
 	public Room (Game game, Random random)	{
 		this.game = game;
 		this.random = random;
 		ent = new ArrayList<Entity>();
-		
+
 		//Generate the room.
 		ent.add(game.hero);
 		game.hero.x = 1;
 		game.hero.y = 1;
-		
+
 		Goblin goblin = new Goblin();
 		goblin.controller = new AttackController(goblin);
-		goblin.x = 14;
-		goblin.y = 14;
+		goblin.x = 6;
+		goblin.y = 6;
 		ent.add(goblin);
-		
-		for (int i = 0; i < 15; i++) {
+
+		for (int i = 0; i < 8; i++) {
 			Wall wall = new Wall();
 			wall.x = i;
 			wall.y = 0;
 			ent.add(wall);
 			wall = new Wall();
-			wall.x = 15;
+			wall.x = 7;
 			wall.y = i;
 			ent.add(wall);
 			wall = new Wall();
-			wall.x = 15 - i;
-			wall.y = 15;
+			wall.x = 7 - i;
+			wall.y = 7;
 			ent.add(wall);
 			wall = new Wall();
 			wall.x = 0;
-			wall.y = 15 - i;
+			wall.y = 7 - i;
 			ent.add(wall);
 		}
 	}
-	
+
 	public void render(GameContainer gc, Graphics g) throws SlickException {
 		for (Entity e : ent) {
 			if (!(e instanceof Actor)) e.render(gc, g);
@@ -58,7 +76,7 @@ public class Room {
 			if (e instanceof Actor) e.render(gc, g);
 		}
 	}
-	
+
 	public void update(GameContainer gc) {
 		this.gc = gc;
 		if (waiting.isEmpty()) {
@@ -68,14 +86,14 @@ public class Room {
 			waiting.remove(waiting.size() - 1);
 		}
 	}
-	
+
 	public boolean checkForTypeAt (int x, int y, Class<?> type) {
 		for (Entity e : ent) {
 			if (e.x == x && e.y == y) {
 				if (e.getClass() == type) return true;
 			}
 		}
-		
+
 		return false;
 	}
 }
