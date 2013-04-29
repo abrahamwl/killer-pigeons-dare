4c123e91123c3e2d9a2323beaa6863f582ce3616
eric.holloway@gmail.com <eric.holloway@gmail.com@0c5cd833-a6f2-db63-4c2d-ceeb27597673>
2013-04-29 11:54:54 +0000

    unused finish class

---
+++ Killer Pigeons Dare/src/game/entity/Finish.java
@@ -0,0 +1,48 @@
+package game.entity;
+
+import game.Actor;
+import game.Entity;
+import game.Room;
+
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
+
+public class Finish extends Entity {
+	private static SpriteSheet sheet = null;
+	public int roomNumber = 1;
+	private int distanceFromCharacterStart;
+	
+	public int getDistanceFromCharacterStart() {
+		return distanceFromCharacterStart;
+	}
+
+	public Finish (int roomNumber) {
+		super("Finish");
+		this.roomNumber = roomNumber;
+		if (sheet == null) {
+			try {
+				sheet = new SpriteSheet("res/fancy_door.png", 64, 64, 0);
+			} catch (SlickException e) {
+				e.printStackTrace();
+			}
+		}
+		image = sheet.getSubImage(0, 0); 
+	}
+
+	@Override
+	public boolean execute(Room r) {
+		return true; //Doesn't do anything.
+	}
+
+	@Override
+	public boolean passableFor(Actor a) {
+		return true;
+	}
+	
+	@Override
+	public void init (Room r) {
+		super.init(r);
+		distanceFromCharacterStart = (int)Math.max(Math.abs(x - r.game.hero.x), Math.abs(y - r.game.hero.y));
+	}
+
+}
