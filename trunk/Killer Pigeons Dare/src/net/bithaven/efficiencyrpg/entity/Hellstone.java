b3369de3548fe35b8a4a924dc5dc25d12fca126d
eric.holloway@gmail.com <eric.holloway@gmail.com@0c5cd833-a6f2-db63-4c2d-ceeb27597673>
2014-05-04 14:42:15 +0000

    Created HellStone that damages actors.  Added Fire Friend ability, so HellStone benefits the holder.  And made a BadLands level generator.

--- trunk/Killer Pigeons Dare/src/net/bithaven/efficiencyrpg/entity/Hellstone.java
+++ trunk/Killer Pigeons Dare/src/net/bithaven/efficiencyrpg/entity/Hellstone.java
@@ -1,7 +1,8 @@
 package net.bithaven.efficiencyrpg.entity;
 
-
 import net.bithaven.efficiencyrpg.Room;
+import net.bithaven.efficiencyrpg.ability.MovementPassabilityModifier;
+import net.bithaven.efficiencyrpg.other.Damage;
 
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
@@ -14,6 +15,12 @@ public class Hellstone extends Entity {
 
 	@Override
 	public boolean execute() {
+		for(Entity e : room.entitiesAt(this.x, this.y, Actor.class))
+			if(((Actor)e).activeAbilities.getPassabilityModifiers().contains(MovementPassabilityModifier.FIRE_FRIEND))
+				((Actor)e).applyHealth(1);
+			else if(!((Actor)e).activeAbilities.getPassabilityModifiers().contains(MovementPassabilityModifier.FLYING))
+				((Actor)e).applyDamage(new Damage(2));
+		
 		return true; //Doesn't do anything.
 	}
 
