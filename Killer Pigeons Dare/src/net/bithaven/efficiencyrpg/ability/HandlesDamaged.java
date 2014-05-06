f02c81fa33e22dcd73cc329681e59ab9c6450bd4
abrahamwl@gmail.com <abrahamwl@gmail.com@0c5cd833-a6f2-db63-4c2d-ceeb27597673>
2014-05-06 05:43:29 +0000

    Renamed HandlesDamage to HandlesDamaged.

--- Killer Pigeons Dare/src/net/bithaven/efficiencyrpg/ability/HandlesDamaged.java
+++ Killer Pigeons Dare/src/net/bithaven/efficiencyrpg/ability/HandlesDamaged.java
@@ -2,6 +2,6 @@ package net.bithaven.efficiencyrpg.ability;
 
 import net.bithaven.efficiencyrpg.other.Damage;
 
-public interface HandlesDamage extends Handler {
+public interface HandlesDamaged extends Handler {
 	public Damage modifyDamage (Damage damage);
 }
